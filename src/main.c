8bd036df471f00d4305462d3b4567bd829305b2a
hrdlickaradek <93579280+hrdlickaradek@users.noreply.github.com>
2022-01-18 11:14:04 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include "stm8s.h"
 #include "milis.h"
+#include "spse_stm8.h"
 
 /*#include "delay.h"*/
-/*#include <stdio.h>*/
-/*#include "../lib/uart.c"*/
+//#include <cstdint>
+#include <stdio.h>
+#include "../lib/uart.c"
 
 #define _ISOC99_SOURCE
 #define _GNU_SOURCE
@@ -24,21 +26,51 @@ void setup(void)
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
     GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
     init_milis();
+    
+    init_uart();
+    
+    // na pinech/vstupech ADC_IN2 (PB2) a ADC_IN3 (PB3) vypneme vstupní buffer
+    ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL4, DISABLE);
+    ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL5, DISABLE);
+
+    // při inicializaci volíme frekvenci AD převodníku mezi 1-4MHz při 3.3V
+    // mezi 1-6MHz při 5V napájení
+    // nastavíme clock pro ADC (16MHz / 4 = 4MHz)
+    ADC2_PrescalerConfig(ADC2_PRESSEL_FCPU_D4);
+
+    // volíme zarovnání výsledku (typicky vpravo, jen vyjmečně je výhodné vlevo)
+    ADC2_AlignConfig(ADC2_ALIGN_RIGHT);
+    
+    // nasatvíme multiplexer na některý ze vstupních kanálů
+    ADC2_Select_Channel(ADC2_CHANNEL_4);
+    // rozběhneme AD převodník
+    ADC2_Cmd(ENABLE);
+    // počkáme než se AD převodník rozběhne (~7us)
+    ADC2_Startup_Wait();
+
 }
 
 
 int main(void)
 {
     uint32_t time = 0;
+    uint32_t voltage = 0;
+    uint32_t temperature = 0;
 
     setup();
-    /*init_uart();*/
+    //init_uart();*/
+    uint16_t ADCx = 0;
 
     while (1) {
 
         if (milis() - time > 333 && BTN_PUSH) {
             LED_TOGG; 
             time = milis();
+            ADCx = ADC_get(ADC2_CHANNEL_4);
+            voltage = (uint32_t) 3300 * ADCx / 1024;
+            temperature = ((uint32_t) 33000 * ADCx -(uint32_t)4096000 + 19968/2) / 19968;
+
+            printf("value = %d, Voltage = %ldmV, temperature =%ld.%ld C \r \n", ADCx, voltage,temperature/10,temperature%10);
         }
 
         /*LED_FLIP; */
